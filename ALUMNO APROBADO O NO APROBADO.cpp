e0ddecb6317fde1acbabdd4bd034d5d405db90db
Angel Manuel Buitrago Montes <angel_m_buitrago@hotmail.com>
2017-05-19 21:01:42 -0500

    Inclucion de ejemplo de archivo

---
+++ ALUMNO APROBADO O NO APROBADO.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+/*Ingrese dos números desde el teclado e imprima ambos números solo */
+/*si son de diferente signo y distintos de cero */
+
+using namespace std;
+
+main() 
+{
+int n1,n2;
+
+
+cout << "introdusca nota del alumno " << endl;
+cin>>n1;
+
+
+if
+(n1 >= 71)
+
+cout << n1 << " alumno aprobado" <<endl;
+else
+cout << n1 << "alumno no aprobado " <<endl;
+      return 0;
+	}
+
